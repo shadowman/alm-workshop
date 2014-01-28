90b3da8c2233ad560ab2aa724a0b30962759de4a
José E. Rodríguez Huerta <jrhuerta@runroom.com>
2014-01-28 21:06:29 +0100

    Installing jenkins

--- puppet/manifests/workshop.pp
+++ puppet/manifests/workshop.pp
@@ -1,15 +1,24 @@
+exec { 'apt-get update':
+	path => "/usr/bin:/bin",
+	command => 'sudo apt-get update',
+	onlyif  => 'find /var/lib/apt/lists -type d -mtime +1 | grep -q /list'
+}
+
+Package {
+	require => Exec['apt-get update'],
+}
+
 class git {
-	exec { 'apt-get update':
-		command => '/usr/bin/sudo apt-get update';
-	}
 	package { "git":
-		ensure => installed,
-		require => Exec['apt-get update'];
+		ensure => installed
 	}
 }
 
 node "ci" {
 	include git	
+	package { "jenkins":
+		ensure => installed
+	}
 }
 
 node "dev" {
