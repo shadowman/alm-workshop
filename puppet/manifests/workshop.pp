9e5295ef06d1c61fb4759247a2d1ef2ede1d6d6e
José E. Rodríguez Huerta <jrhuerta@runroom.com>
2014-01-28 20:44:44 +0100

    Adding ci and dev machines and puppet configurations

--- puppet/manifests/workshop.pp
+++ puppet/manifests/workshop.pp
@@ -1,1 +1,20 @@
-workshop
+class git {
+	exec { 'apt-get update':
+		command => '/usr/bin/sudo apt-get update';
+	}
+	package { "git":
+		ensure => installed,
+		require => Exec['apt-get update'];
+	}
+}
+
+node "ci" {
+	include git	
+}
+
+node "dev" {
+	include git
+}
+
+
+
