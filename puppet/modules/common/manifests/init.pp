63d6a1f844f0a7c4f4798b39887ba8d4811a96aa
Alex Muntada <alexm@alexm.org>
2014-01-31 23:33:59 +0100

    Fixing hosts mode and apt update check

--- puppet/modules/common/manifests/init.pp
+++ puppet/modules/common/manifests/init.pp
@@ -4,7 +4,7 @@
 exec { 'apt-get update':
   path    => '/usr/bin:/bin',
   command => 'sudo apt-get update',
-  onlyif  => 'find /var/lib/apt/lists -type d -mtime +1 | grep -q /list'
+  onlyif  => 'find /var/lib/apt/lists -type d -mtime +1 | grep -q /lists'
 }
 
 #
@@ -23,7 +23,7 @@ class common {
   file { '/etc/hosts':
     ensure => present,
     source => '/vagrant/puppet/files/hosts',
-    mode   => '0664',
+    mode   => '0644',
     owner  => root,
     group  => root
   }
